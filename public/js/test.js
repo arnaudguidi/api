9d0f1cfcd987d0484e8b54619cf94d15d6e7a09f
arnaudguidi <33958486+arnaudguidi@users.noreply.github.com>
2021-09-16 12:01:32 +0200

    for api-key

--- public/js/test.js
+++
@@ -1,9 +0,0 @@
-
-// when the DOM is ready
-function ready(callback) {
-    if (document.readyState != 'loading') callback(); else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
-    else document.attachEvent('onreadystatechange', function () {if (document.readyState == 'complete') callback();});
-}
-ready(function () {
-    console.log("**** Test d'un import de JS from the google :D ******");
-});
